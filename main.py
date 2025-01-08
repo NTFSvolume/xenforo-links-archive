2ac8921f46fc3e8eae5775f3f75ec1accd90ced5
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-01-08 08:25:04 -0500

    refactor: allow saving non thread URLs

--- main.py
+++ main.py
@@ -48,25 +48,48 @@ class ForumThread:
         self.thread_path = "/" + "/".join(self.url.parts[1:name_index+1])
 
     @property
-    def as_tuple(self) -> tuple [str, str, str, int]:
-        return self.url.host, self.url.path, self.name, self.page # type: ignore
+    def as_tuple(self) -> tuple [str, str, str, int, int]:
+        return self.url.host, self.name, self.page, self.post_number, self.url.path_qs # type: ignore
 
 def save_data(data: DatabaseEntry) -> None:
-    forum_thread = ForumThread(data.origin)
+    try:
+        forum_thread = ForumThread(data.origin)
+        save_forum_thread_urls(forum_thread, data.urls)
+    except ValueError:
+        save_other_urls(data.origin, data.urls)
+
+def save_forum_thread_urls(forum_thread: ForumThread, urls:list[str]) -> None:
     conn = sqlite3.connect("data.db")
     cursor = conn.cursor()
     date_received = datetime.now(UTC).isoformat()
-    query = "INSERT OR IGNORE INTO forum_threads (host, path, name, page, url, date) VALUES (?, ?, ?, ?, ?, ?)"
-    for url in data.urls:
+    query = "INSERT OR IGNORE INTO forum_threads (host, name, page, post, path_qs, url, date) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    for url in urls:
         cursor.execute(query,(*forum_thread.as_tuple, url, date_received))
+    conn.commit()
+    conn.close()
+
+def save_other_urls(origin: HttpUrl, urls:list[str]) -> None:
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+    date_received = datetime.now(UTC).isoformat()
+    query = "INSERT OR IGNORE INTO other_urls (origin, url, date) VALUES (?, ?, ?)"
+    for url in urls:
+        cursor.execute(query,(origin, url, date_received))
     conn.commit()
     conn.close()
 
 
 def get_urls_by_origin(origin: HttpUrl, page: int | None = None):
+    try:
+        forum_thread = ForumThread(origin)
+        return get_forum_thread_urls(forum_thread, page)
+    except ValueError:
+        return get_other_urls(origin)
+
+
+def get_forum_thread_urls(forum_thread: ForumThread, page: int | None = None):
     conn = sqlite3.connect("data.db")
     cursor = conn.cursor()
-    forum_thread = ForumThread(origin)
     data = forum_thread.as_tuple[0:3]
     query = "SELECT url FROM forum_threads WHERE host = ? AND path = ? AND name = ?"
     if page:
@@ -74,6 +97,16 @@ def get_urls_by_origin(origin: HttpUrl, page: int | None = None):
         data = forum_thread.as_tuple
 
     cursor.execute(query, (*data,))
+    rows = cursor.fetchall()
+    conn.close()
+    return [row[0] for row in rows]
+
+
+def get_other_urls(origin: HttpUrl):
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+    query = "SELECT url FROM other_urls WHERE origin = ?"
+    cursor.execute(query, (origin,))
     rows = cursor.fetchall()
     conn.close()
     return [row[0] for row in rows]
@@ -102,13 +135,22 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS forum_threads (
             host TEXT,
-            path TEXT,
             name TEXT,
             page NUMBER,
             post NUMBER,
             url TEXT,
             date TEXT,
-            UNIQUE(host, path, url)
+            path_qs TEXT,
+            UNIQUE(host, name, page, url)
+        )
+    """)
+    conn.commit()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS other_urls (
+            origin TEXT,
+            url TEXT,
+            date TEXT,
+            UNIQUE(host, name, page, url)
         )
     """)
     conn.commit()
